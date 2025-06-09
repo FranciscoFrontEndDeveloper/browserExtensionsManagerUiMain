35decaa978bc0c0c2011c4bb8e134a4b276bda95
FRANCISCO <pachonetworking@gmail.com>
2025-06-08 19:59:29 -0500

    styles card paragraph

--- src/mockExtensions.ts
+++ src/mockExtensions.ts
@@ -1,62 +1,62 @@
 export const EXTENSIONES: { title: string; text: string; img: string }[] = [
- {
-  img:'assets/images/logo-devlens.svg',
-  title: 'DevLens',
-  text: 'Quickly inspect page layouts and visualize element boundaries.'  
+  {
+    img: 'assets/images/logo-devlens.svg',
+    title: 'DevLens',
+    text: 'Quickly inspect page layouts and visualize element boundaries.',
   },
- {
-    img:'assets/images/logo-style-spy.svg',
+  {
+    img: 'assets/images/logo-style-spy.svg',
     title: 'StyleSpy',
-    text: 'Instantly analyze and copy CSS from any webpage element.'
+    text: 'Instantly analyze and copy CSS from any webpage element.',
   },
- { 
-    img:'assets/images/logo-speed-boost.svg',
+  {
+    img: 'assets/images/logo-speed-boost.svg',
     title: 'SpeedBoost',
-    text: 'Optimizes browser resource usage to accelerate page loading.'
+    text: 'Optimizes browser resource usage to accelerate page loading.',
   },
- {
-    img:'assets/images/logo-json-wizard.svg',
+  {
+    img: 'assets/images/logo-json-wizard.svg',
     title: 'JSONWizard',
-    text: 'Formats, validates, and prettifies JSON responses in-browser.'
+    text: 'Formats, validates, and prettifies JSON responses in-browser.',
   },
   {
-    img:'assets/images/logo-tab-master-pro.svg', 
+    img: 'assets/images/logo-tab-master-pro.svg',
     title: 'TabMaster Pro',
-    text: 'Organizes browser tabs into groups and sessions.'
+    text: 'Organizes browser tabs into groups and sessions.',
   },
   {
-   img:'assets/images/logo-viewport-buddy.svg', 
-   title: 'ViewportBuddy',
-   text: 'Simulates various screen resolutions directly within the browser.'
+    img: 'assets/images/logo-viewport-buddy.svg',
+    title: 'ViewportBuddy',
+    text: 'Simulates various screen resolutions directly within the browser.',
   },
- {
-   img:'assets/images/logo-markup-notes.svg',
-   title: 'Markup Notes',
-   text: 'Enables annotation and notes directly onto webpages for collaborative debugging.'
+  {
+    img: 'assets/images/logo-markup-notes.svg',
+    title: 'Markup Notes',
+    text: 'Enables annotation and notes directly onto webpages for collaborative debugging.',
   },
- {
-   img:'assets/images/grid-guides.svg',
-   title: 'GridGuides',
-   text: 'Overlay customizable grids and alignment guides on any webpage.'
+  {
+    img: 'assets/images/logo-grid-guides.svg',
+    title: 'GridGuides',
+    text: 'Overlay customizable grids and alignment guides on any webpage.',
   },
- {
-   img:'assets/images/logo-palette-picker.svg',
-   title: 'Palette Picker',
-   text: 'Instantly extracts color palettes from any webpage.'
+  {
+    img: 'assets/images/logo-palette-picker.svg',
+    title: 'Palette Picker',
+    text: 'Instantly extracts color palettes from any webpage.',
   },
- {
-   img:'assets/images/logo-link-checker.svg',
-   title: 'LinkChecker',
-   text: 'Scans and highlights broken links on any page.'
+  {
+    img: 'assets/images/logo-link-checker.svg',
+    title: 'LinkChecker',
+    text: 'Scans and highlights broken links on any page.',
   },
- {
-   img:'assets/images/logo-dom-snapshot',
-   title: 'DOM Snapshot',
-   text: 'Capture and export DOM structures quickly.'
+  {
+    img: 'assets/images/logo-dom-snapshot.svg',
+    title: 'DOM Snapshot',
+    text: 'Capture and export DOM structures quickly.',
   },
- {
-   img:'assets/images/logo-console-plus.svg',
-   title: 'ConsolePlus',
-   text: 'Enhanced developer console with advanced filtering and logging.'
-  }
+  {
+    img: 'assets/images/logo-console-plus.svg',
+    title: 'ConsolePlus',
+    text: 'Enhanced developer console with advanced filtering and logging.',
+  },
 ];
