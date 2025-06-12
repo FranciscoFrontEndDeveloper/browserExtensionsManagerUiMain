74197c9d10877ecb5c44f66a82e9c513f98ce9da
FRANCISCO <pachonetworking@gmail.com>
2025-06-11 19:40:47 -0500

    dark mode init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardextensionComponent } from "./components/cardextension/cardextension.component";
-import { ButtoncompComponent } from "./components/buttoncomp/buttoncomp.component";
-
+import { CardextensionComponent } from './components/cardextension/cardextension.component';
+import { ButtoncompComponent } from './components/buttoncomp/buttoncomp.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardextensionComponent, ButtoncompComponent],
+  imports: [
+    RouterOutlet,
+    CardextensionComponent,
+    ButtoncompComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'browserExtensionsManagerUiMain';
   All: string = 'All';
   Active: string = 'Active';
   Inactive: string = 'Inactive';
+  isDarkMode: boolean = true;
   darkmode() {
-    console.log('dark mode')
+    this.isDarkMode = !this.isDarkMode;
   }
 }
