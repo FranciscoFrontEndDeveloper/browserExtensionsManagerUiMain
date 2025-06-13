0b62558bd8292da667a050d3b68fbbdb7af5a4a0
FRANCISCO <pachonetworking@gmail.com>
2025-06-12 20:13:07 -0500

    darkmode buttons main finished

--- src/app/components/buttoncomp/buttoncomp.component.ts
+++ src/app/components/buttoncomp/buttoncomp.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-buttoncomp',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './buttoncomp.component.html',
   styleUrl: './buttoncomp.component.sass',
 })
 export class ButtoncompComponent {
-  @Input('buttonReUse') textButton = 'textos'
+  @Input('buttonReUse') textButton = 'textos' 
+  @Input('darkModeButton') darkModeButton = true
+
+  prueba() {
+    console.group('ButtoncompComponent');
+    console.log(this.darkModeButton);
+    console.groupEnd();
+  }
+  
 }
